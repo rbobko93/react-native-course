718576411ce9b26ce481b3ea22bc52e0a4d301c8
rbobko <rafal.bobko@bdpint.com>
2018-11-07 12:29:13 -0500

    Filling in reducer with actions

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,5 @@
+import {ADD_PLACE, DELETE_PLACE, DESELECT_PLACE, SELECT_PLACE} from "../actions/actionTypes";
+
 const initialState = {
   places: [],
   selectedPlace: null
@@ -5,6 +7,37 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random(),
+                    name: action.placeName,
+                    image: {
+                        uri: "https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    }
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey;
+                })
+            };
+        case DESELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+            };
         default:
             return state;
     }
