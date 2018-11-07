29205c00c57ee240f876ef0ebebff588e5711eff
rbobko <rafal.bobko@bdpint.com>
2018-11-07 11:33:50 -0500

    Adding modal for places.

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from "./src/components/PlaceList/PlaceList";
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
 
 export default class App extends React.Component {
     state = {
-        places: []
+        places: [],
+        selectedPlace: null
     };
 
     placeAddedHandler = placeName => {
@@ -22,21 +24,43 @@ export default class App extends React.Component {
         })
     };
 
-    placeDeletedHandler = key => {
+    placeSelectedHandler = key => {
+        this.setState(prevState => {
+            return {
+                selectedPlace: prevState.places.find(place => {
+                    return place.key === key;
+                })
+            };
+        })
+    };
+
+    placeDeletedHandler = () => {
         this.setState(prevState => {
             return {
                 places: prevState.places.filter(place => {
-                    return place.key !== key;
-                })
-            }
+                    return place.key !== prevState.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        });
+    };
+
+    modalClosedHandler = () => {
+        this.setState({
+            selectedPlace: null
         });
     };
 
     render() {
         return (
             <View style={styles.container}>
+                <PlaceDetail
+                    selectedPlace={this.state.selectedPlace}
+                    onItemDeleted={this.placeDeletedHandler}
+                    onModalClosed={this.modalClosedHandler}
+                />
                 <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-                <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler}/>
+                <PlaceList places={this.state.places} onItemSelected={this.placeSelectedHandler}/>
             </View>
         );
     }
--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -9,7 +9,7 @@ const placeList = props => {
             <ListItem
                 placeName={info.item.name}
                 placeImage={info.item.image}
-                onItemPressed={() => props.onItemDeleted(info.item.key)}/>
+                onItemPressed={() => props.onItemSelected(info.item.key)}/>
         )}/>
     );
 };
